e066cc99b4ab0487449c9e72444191f255c599d1
Timothy Fee <keys333@proton.me>
2024-09-29 21:36:31 -0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
+    //Ask user for age, if user is 21 or older then display they get a wristband.
+    Scanner in = new Scanner(System.in);
+    int age = 0;
 
-    double initialBalance = 11438.22;
-    double interestRate = 0.17;
-    double firstMonth = initialBalance * interestRate;
-    double secondMonth = (initialBalance + firstMonth) * interestRate;
-
-    System.out.println("Balance after 1 month: " + (initialBalance + firstMonth));
-    System.out.println("Balance after 2 months: " + (initialBalance + firstMonth + secondMonth));
+    System.out.print("Enter your age: ");
+    age = in.nextInt();
+    
+    if (age >= 21) {
+      System.out.println("You get a wrist band!");
+    }
     
   }
 }
